85ceb53d82ecffcd2e1cbe569085f45615e06ebc
Riku Kawano <rrrikukawano@gmail.com>
2016-06-16 08:07:40 +0000

    age validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
     validates :name, length: { maximum: 20 }, presence: true
     validates :body, length: { minimum: 2, maximum: 30 }, presence: true
+    validates :age, inclusion: { in: 0..100 }, presence: true
 end
